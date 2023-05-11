997822b52abcb081416e32504db2be5523200cfd
John Dutchover <johndutchover@users.noreply.github.com>
2023-05-10 22:37:36 -0400

    comments for poetry

--- src/frontend/short_circuit_me.py
+++ src/frontend/short_circuit_me.py
@@ -10,4 +10,8 @@ st.title('Notification Overview Dashboard')
 
 st.text(f'You got {get_important_notifications()} important notifications.')
 
-# run this file via: streamlit run short_circuit_me.py
+'''To run this file with poetry here are the commands:
+        poetry shell
+        streamlit run short_circuit_me.py
+    Then type "code ." to launch VSCode
+'''
