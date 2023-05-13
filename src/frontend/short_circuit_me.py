1aa94e062e77f3e26231d62f7571dca70bcfb66a
John Dutchover <johndutchover@users.noreply.github.com>
2023-05-12 23:02:08 -0400

    PEP8

--- src/frontend/short_circuit_me.py
+++ src/frontend/short_circuit_me.py
@@ -6,12 +6,13 @@ import streamlit as st
 def get_important_notifications():
     return 42
 
+
 st.title('Notification Overview Dashboard')
 
 st.text(f'You got {get_important_notifications()} important notifications.')
 
 '''To run this file:
-        - from VSCode, use "Run Python File" insead of "Run Code" (properly activates venv)
+        - from VSCode, use "Run Python File" instead of "Run Code" (properly activates venv)
         - from PyCharm...it will probably just work fine.
         - from external terminal, use "poetry shell" followed by:
             streamlit run short_circuit_me.py
