9fdaf079db096bd7c35b507e33ec9e676f2cdd6c
John Dutchover <johndutchover@users.noreply.github.com>
2023-05-15 23:01:22 -0400

    layout

--- scm-poetry/streamlit_app.py
+++ scm-poetry/streamlit_app.py
@@ -5,7 +5,7 @@ import streamlit as st
 st.set_page_config(
     page_title="Simple Dashboard",
     page_icon="✅",
-    layout="wide",
+    layout="centered",
 )
 
 def get_important_notifications():
@@ -16,9 +16,10 @@ st.title('Notification Overview Dashboard')
 
 st.text(f'You got {get_important_notifications()} important notifications.')
 
-'''To run this file:
-        - from VSCode, use "Run Python File" instead of "Run Code" (properly activates venv)
-        - from PyCharm...it will probably just work fine.
-        - from external terminal, use "poetry shell" followed by:
-            streamlit run short_circuit_me.py
+'''
+To run this file:
+- from VSCode, use "Run Python File" instead of "Run Code" (properly activates venv)
+- from PyCharm...set default python interpreter to venv
+- from external terminal, use "poetry shell" followed by:
+  - streamlit run streamlit_app.py
 '''
