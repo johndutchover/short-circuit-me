3469213b1d4dc25ea55ed37877586dd0ab3c6b15
John Dutchover <johndutchover@users.noreply.github.com>
2023-05-16 22:05:29 -0400

    dataframe, widget

--- scm-poetry/streamlit_app.py
+++ scm-poetry/streamlit_app.py
@@ -1,6 +1,12 @@
 # https://docs.streamlit.io/library/get-started/create-an-app
+# .io/how-to-build-a-real-time-live-dashboard-with-streamlit/#2-how-to-do-a-basic-dashboard-setup
+
+#If you are using Streamlit version 1.10.0 or higher, your main script should live in a directory other than the root directory. 
+# When using Docker, you can use the WORKDIR command to specify the directory where your main script lives.
 
 import streamlit as st
+import pandas as pd
+import numpy as np
 
 st.set_page_config(
     page_title="Simple Dashboard",
@@ -11,10 +17,20 @@ st.set_page_config(
 def get_important_notifications():
     return 42
 
+df = pd.DataFrame({
+  'Important messages': [1, 2, 3, 4],
+  'Total messages': [10, 20, 30, 40]
+})
 
+# page title
 st.title('Notification Overview Dashboard')
-
+# body
 st.text(f'You got {get_important_notifications()} important notifications.')
+# call dataframe
+df
+# widget (slider)
+x = st.slider('x') 
+st.write(x, 'Short-circuit-me power level is', x * x)
 
 #'''To run this file:
 #- from VSCode, use "Run Python File" instead of "Run Code" (properly activates venv)
