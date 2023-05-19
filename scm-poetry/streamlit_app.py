4eb29558c5fd44f9f9704071c50c31429c1fc42e
John Dutchover <johndutchover@users.noreply.github.com>
2023-05-19 14:18:32 -0400

    add bokeh graph

--- scm-poetry/streamlit_app.py
+++ scm-poetry/streamlit_app.py
@@ -18,6 +18,9 @@ st.set_page_config(
 def get_important_notifications():
     return 42
 
+def get_critical_notifications():
+    return 7
+
 df = pd.DataFrame({
   'Important messages': [1, 2, 3, 4],
   'Total messages': [10, 20, 30, 40]
@@ -25,22 +28,37 @@ df = pd.DataFrame({
 
 df_weekly = pd.DataFrame({
   'important_messages': [1, 2, 3, 4, 3, 2, 3],
-  'normal_messages': [10, 20, 30, 40, 220, 29, 30],
+  'normal_messages': [10, 20, 30, 40, 22, 29, 30],
   'critical_messages': [0, 2, 3, 4, 3, 4, 3],
   'days': ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
 })
 
+# prepare some data
+x1 = [10, 20, 30, 40, 30, 50, 40]
+y1 = [1, 2, 3, 4, 3, 2, 3]
+y2 = [10, 20, 30, 40, 22, 29, 30]
+y3 = [0, 2, 3, 4, 3, 4, 3]
+
 # page title
 st.title('Notification Overview Dashboard')
 # body
-st.text(f'You got {get_important_notifications()} important notifications.')
-# call dataframe
-df
-# widget (slider)
-x = st.slider('x') 
-st.write(x, 'Short-circuit-me power level is', x * x)
+st.text(f'You got {get_important_notifications()} important and {get_critical_notifications()} critical notifications.')
 
-# bar chart
+# widget (slider)
+values = st.slider(
+    'Select a range of messages to include',
+    0.0, 100.0, (25.0, 75.0))
+st.write('Values:', values)
+
+# bokeh: create a new plot with a title and axis labels
+p = figure(title="Bokeh plot", x_axis_label='messages', y_axis_label='day')
+# add multiple renderers
+p.line(x1, y1, legend_label="Important", color="blue", line_width=2)
+p.line(x1, y2, legend_label="Normal", color="green", line_width=2)
+p.line(x1, y3, legend_label="Critical", color="red", line_width=2)
+st.bokeh_chart(p, use_container_width=True)
+
+# st.area chart
 st.area_chart(
     df_weekly,
     x="important_messages",
