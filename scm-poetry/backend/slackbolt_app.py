c0713b4ad9a63d58777d64283ccc773d4d5ae05b
John Dutchover <johndutchover@users.noreply.github.com>
2023-05-30 15:40:31 -0400

    Bolt app is running using .env

--- scm-poetry/backend/slackbolt_app.py
+++ scm-poetry/backend/slackbolt_app.py
@@ -1,23 +1,23 @@
 # https://api.slack.com/apps/A059F0MBC4Q
 import os
 from dotenv import load_dotenv
-
-# Use the package we installed
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
 
 # load dotenv
 load_dotenv()
 
+# Initializes your app with your bot token and socket mode handler
+app = App(token=os.environ.get("POETRY_SCM_XOXB_TOKEN"))
+
 # Initializes app-level (xapp) token and signing secret
-slackboltapp = App(
-    token=os.environ.get("POETRY_SCM_XOXB_TOKEN"),
-    # signing_secret=os.environ.get("POETRY_SCM_BOT_SIGNINGSECRET") # not required for socket mode
-)
+# slackboltapp = App(
+#    token=os.environ.get("POETRY_SCM_XOXB_TOKEN"),
+#    # signing_secret=os.environ.get("POETRY_SCM_BOT_SIGNINGSECRET") # not required for socket mode
+# )
 
 # Add functionality here
 # @app.event("app_home_opened") etc
-
 
 # Request URLs (development use)
 # if __name__ == "__main__":
@@ -25,4 +25,4 @@ slackboltapp = App(
 
 # using WebSockets
 if __name__ == "__main__":
-    SocketModeHandler(App, os.environ["POETRY_SCM_XAPP_TOKEN"]).start()
+    SocketModeHandler(app, os.environ["POETRY_SCM_XAPP_TOKEN"]).start()
