bd655981a81b71d4a9c5530933295944bc97c730
John Dutchover <johndutchover@users.noreply.github.com>
2023-05-29 20:10:25 -0400

    slackbolt_app.py WebSockets

--- scm-poetry/backend/slackbolt_app.py
+++ scm-poetry/backend/slackbolt_app.py
@@ -2,17 +2,22 @@
 import os
 # Use the package we installed
 from slack_bolt import App
+from slack_bolt.adapter.socket_mode import SocketModeHandler
 
 # Initializes app-level (xapp) token and signing secret
 slackboltapp = App(
-    token=os.environ.get("POETRY_SLACK_BOT_TOKEN"),
-    signing_secret=os.environ.get("POETRY_SLACK_SIGNING_SECRET")
+    token=os.environ.get("POETRY_SCM_XOXB_TOKEN"),
+    signing_secret=os.environ.get("POETRY_SCM_BOT_SIGNINGSECRET")
 )
 
 # Add functionality here
 # @app.event("app_home_opened") etc
 
 
-# Start your app
+# Request URLs (development use)
+#if __name__ == "__main__":
+#    slackboltapp.start(port=int(os.environ.get("PORT", 3000)))
+
+# using WebSockets
 if __name__ == "__main__":
-    slackboltapp.start(port=int(os.environ.get("PORT", 3000)))
+    SocketModeHandler(App, os.environ["POETRY_SCM_XAPP_TOKEN"]).start()
