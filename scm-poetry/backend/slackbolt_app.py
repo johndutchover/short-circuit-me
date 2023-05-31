533c34eabb677d3e0007482ad951d0832619de82
John Dutchover <johndutchover@users.noreply.github.com>
2023-05-31 11:47:44 -0400

    introduce message() method

--- scm-poetry/backend/slackbolt_app.py
+++ scm-poetry/backend/slackbolt_app.py
@@ -10,14 +10,21 @@ load_dotenv()
 # Initializes your app with your bot token and socket mode handler
 app = App(token=os.environ.get("POETRY_SCM_XOXB_TOKEN"))
 
+
 # Initializes app-level (xapp) token and signing secret
 # slackboltapp = App(
 #    token=os.environ.get("POETRY_SCM_XOXB_TOKEN"),
 #    # signing_secret=os.environ.get("POETRY_SCM_BOT_SIGNINGSECRET") # not required for socket mode
 # )
 
-# Add functionality here
-# @app.event("app_home_opened") etc
+# Listens to incoming messages that contain "hello"
+# To learn available listener arguments,
+# visit https://slack.dev/bolt-python/api-docs/slack_bolt/kwargs_injection/args.html
+@app.message("hello")
+def message_hello(message, say):
+    # say() sends a message to the channel where the event was triggered
+    say(f"Hey there <@{message['user']}>!")
+
 
 # Request URLs (development use)
 # if __name__ == "__main__":
