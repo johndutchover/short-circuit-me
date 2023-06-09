52d1a7014894707a3ee1e51265741bd2b9edcc4f
John Dutchover <johndutchover@users.noreply.github.com>
2023-06-08 21:30:44 -0400

    pymongo

--- scm-poetry/backend/slackbolt_app.py
+++ scm-poetry/backend/slackbolt_app.py
@@ -43,7 +43,7 @@ client = MongoClient(uri, server_api=ServerApi('1'))
 # Send a ping to confirm a successful connection
 try:
     client.admin.command('ping')
-    print("Pinged your deployment. You successfully connected to MongoDB!")
+    #print("Pinged your deployment. You successfully connected to MongoDB!")
 except Exception as e:
     print(e)
 
