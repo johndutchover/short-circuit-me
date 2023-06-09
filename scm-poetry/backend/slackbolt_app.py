8fb31df0a292ced2fecb529df5d7ccbc5eeab279
John Dutchover <johndutchover@users.noreply.github.com>
2023-06-09 17:53:24 -0400

    more path stuff need a break

--- scm-poetry/backend/slackbolt_app.py
+++ scm-poetry/backend/slackbolt_app.py
@@ -74,7 +74,7 @@ async def endpoint(req: Request):
 
 
 def increase_counter(message_type: str):
-    message_counts_df: DataFrame | Any = pd.read_csv("data/message_counts.csv", index_col=0)
+    message_counts_df: DataFrame | Any = pd.read_csv("../data/message_counts.csv", index_col=0)
 
     now = datetime.datetime.now()
     formatted_date = now.strftime("%Y-%m-%d")
@@ -83,7 +83,7 @@ def increase_counter(message_type: str):
         message_counts_df.loc[formatted_date] = [0, 0, 0]
 
     message_counts_df.loc[formatted_date, message_type] += 1
-    message_counts_df.to_csv("data/message_counts.csv")
+    message_counts_df.to_csv("../data/message_counts.csv")
 
 
 counter = 0
