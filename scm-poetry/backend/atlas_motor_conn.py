98123d34ddc0e59d67ebf31f8df321dd5131bbe4
John Dutchover <johndutchover@users.noreply.github.com>
2023-06-09 20:40:10 -0400

    atlas_motor_conn.py

--- scm-poetry/backend/atlas_motor_conn.py
+++ scm-poetry/backend/atlas_motor_conn.py
@@ -1,19 +1,24 @@
 import os
 
-import motor
+import asyncio
 from motor.motor_asyncio import AsyncIOMotorClient
+from pymongo.server_api import ServerApi
 from dotenv import load_dotenv
 
 load_dotenv()  # read local .env file
-uri = os.environ.get("POETRY_MONGODB_URL")
 
-# Create a new client and connect to the server
-# client = MongoClient(uri, server_api=ServerApi('1'))
-client = motor.motor_asyncio.AsyncIOMotorClient(os.environ["POETRY_MONGODB_URL"])
 
-# Send a ping to confirm a successful connection
-try:
-    client.admin.command('ping')
-    print("Pinged your deployment. You successfully connected to MongoDB!")
-except Exception as e:
-    print(e)
+async def ping_server():
+    # Replace the placeholder with your Atlas connection string
+    uri = os.environ.get("POETRY_MONGODB_URL")
+    # Set the Stable API version when creating a new client
+    client = AsyncIOMotorClient(uri, server_api=ServerApi('1'))
+    # Send a ping to confirm a successful connection
+    try:
+        client.admin.command('ping')
+        print("Pinged your deployment. Successfly connected to MongoDB using asyncio!")
+    except Exception as e:
+        print(e)
+
+
+asyncio.run(ping_server())
