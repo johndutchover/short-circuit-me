69cd1e84e48742d585277efabe340116d5371d36
John Dutchover <johndutchover@users.noreply.github.com>
2023-06-19 14:29:31 -0400

    refactor

--- scm-poetry/fullstack/start.sh
+++ scm-poetry/fullstack/start.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 python slackbolt_csv.py &
-streamlit run pages/1_home.py --server.port=8501 --server.address=0.0.0.0
+streamlit run pages/1_app.py --server.port=8501 --server.address=0.0.0.0
