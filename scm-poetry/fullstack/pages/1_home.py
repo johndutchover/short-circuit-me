18e2b4613575423d1bbd0be8bc04ad21a483ffea
John Dutchover <johndutchover@users.noreply.github.com>
2023-06-18 22:29:28 -0400

    fixes

--- scm-poetry/fullstack/pages/1_home.py
+++ scm-poetry/fullstack/pages/1_home.py
@@ -1,3 +1,5 @@
+import pathlib
+
 import pandas as pd
 import streamlit as st
 from bokeh.models import FuncTickFormatter
@@ -6,6 +8,7 @@ from dotenv import load_dotenv
 from pandas.errors import EmptyDataError
 from datetime import datetime
 import pytz
+import os
 
 current_utc_time = datetime.utcnow()
 local_timezone = pytz.timezone('America/New_York')  # Replace 'America/New_York' with your desired time zone
@@ -17,7 +20,8 @@ st.write('Current local time:', current_local_time)
 
 load_dotenv('../.env')
 
-message_counts_path = "/app/data/message_counts.csv"
+cfd = pathlib.Path(__file__).parent.parent
+message_counts_path = os.getenv('MESSAGE_COUNTS_PATH', cfd / "message_counts.csv")
 
 
 def get_important_notifications():
@@ -38,13 +42,13 @@ try:
     df_messages['msg_date'] = pd.to_datetime(df_messages['msg_date'])
     st.table(df_messages)
 
-    # bokeh: create a new plot with a title and axis labels
+    # Bokeh: create a new plot with a title and axis labels
     p = figure(title="Bokeh plot", x_axis_label='Date', y_axis_label='Messages', x_axis_type="datetime")
     p.line(df_messages['msg_date'], df_messages['normal'], legend_label="Normal", color="green", line_width=2)
     p.line(df_messages['msg_date'], df_messages['important'], legend_label="Important", color="orange", line_width=2)
     p.line(df_messages['msg_date'], df_messages['urgent'], legend_label="Critical", color="red", line_width=2)
 
-    # Define custom tick formatter to display day of week
+    # Define custom tick formatter to display day of the week
     code = """
     var days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
     var date = new Date(tick);
@@ -54,8 +58,10 @@ try:
     """
     p.xaxis.formatter = FuncTickFormatter(code=code)
 
+    # Display the Bokeh plot using Streamlit
     st.bokeh_chart(p, use_container_width=True)
 
+    # Plot an area chart
     st.area_chart(df_messages.set_index('msg_date')[['normal', 'important', 'urgent']])
 
 except EmptyDataError:
