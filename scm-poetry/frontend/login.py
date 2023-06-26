b667114c05547e5cdf9f4f36a772798bb7759663
John Dutchover <johndutchover@users.noreply.github.com>
2023-06-25 20:58:27 -0400

    alternate pathlib

--- scm-poetry/frontend/login.py
+++ scm-poetry/frontend/login.py
@@ -1,10 +1,11 @@
 import os
-
+import pathlib
 import streamlit as st
 from dotenv import load_dotenv
 from streamlit_extras.switch_page_button import switch_page
 
-load_dotenv('.env')  # TODO add pathlib
+env_path = pathlib.Path(__file__).parent / ".env"
+load_dotenv(env_path)
 
 
 # Check password function
@@ -23,7 +24,7 @@ def check_password():
 
 
 # Prompt for password
-if check_password():  # TODO: add user-based login
+if check_password():
     # Set a flag in the session state to indicate that the password is correct
     st.session_state["password_correct"] = True
     # Redirect using extras switch_page
