7c7435fa64fc4a0d1f50c4ce41c90e3a840b1e71
John Dutchover <johndutchover@users.noreply.github.com>
2023-07-07 21:44:14 -0400

    Fri checkin

--- backend/slackbolt_api.py
+++ backend/slackbolt_api.py
@@ -40,17 +40,22 @@ db = client["messagesdb"]
 collection = db["slackcoll"]
 
 
-# Define a button click action
+# API Endpoint - FastAPI
+@api.post("/slack/events")
+async def endpoint(req: Request):
+    return await app_handler.handle(req)
+
+
+# Slack ACTION Handler
 @bolt.action("click_button_notify")
-async def handle_button_click(ack, body):
+async def handle_button_click(ack, body):  # method is a callback for Slack button action
     # Acknowledge the action request
     ack()
 
     # Extract information from the action payload
     user_id = body["user"]["id"]
 
-    # Perform any desired action here
-    # For example, you can send a message in response to the button click
+    # perform action
     await bolt.client.chat_postMessage(
         channel=user_id,
         text="Notify Button clicked!",
@@ -66,19 +71,102 @@ async def handle_button_click(ack, body):
     )
 
 
-@api.post("/slack/events")
-async def endpoint(req: Request):
-    return await app_handler.handle(req)
+# Slack EVENT Handler
+@bolt.event("message")
+async def message_escalate(message, say):
+    await say(
+        blocks=[
+            {
+                "type": "section",
+                "text": {"type": "mrkdwn", "text": f"Do you need to escalate this <@{message['user']}>?"},
+                "accessory": {
+                    "type": "button",
+                    "text": {"type": "plain_text", "text": "Click to escalate"},
+                    "action_id": "click_button_notify"
+                }
+            }
+        ],
+        text=f"Message has been escalated <@{message['user']}>!"
+    )
+    await increase_counter('urgent')
 
 
-# Slash command help-slack-bolt
+# Slack EVENT Handler
+@bolt.event("message")
+async def check_starred(context: AsyncBoltContext, message: dict, starredcontacts: AsyncWebClient):
+    bot_id = context.bot_user_id
+    # Get the list of items starred by the bot
+    result = await starredcontacts.stars_list(user=bot_id)
+    # Check if the received message is in the list of starred items
+    for item in result['items']:
+        if 'message' in item and item['message']['ts'] == message['ts']:
+            print("starred")
+            await increase_counter_based_on_user_id(user_id=bot_id)
+
+
+# Slack MESSAGE Handler: urgent
+@bolt.message(re.compile("(asap|help|critical)", re.I))
+async def message_urgent(message, say):
+    await say(
+        blocks=[
+            {
+                "type": "section",
+                "text": {"type": "mrkdwn", "text": f"Do you need help <@{message['user']}>?"},
+                "accessory": {
+                    "type": "button",
+                    "text": {"type": "plain_text", "text": "Click to escalate"},
+                    "action_id": "click_button_notify"
+                }
+            }
+        ],
+        text=f"Hey there <@{message['user']}>!"
+    )
+    await increase_counter('urgent')
+
+
+# Slack MESSAGE Handler: priority
+@bolt.message(re.compile("(important|urgent|soon)", re.I))
+async def message_priority(message, say):
+    await say(
+        blocks=[
+            {
+                "type": "section",
+                "text": {"type": "mrkdwn", "text": f"<@{message['user']}> Can it wait until Monday?"},
+                "accessory": {
+                    "type": "button",
+                    "text": {"type": "plain_text", "text": "Click to record a message"},
+                    "action_id": "click_button_notify"
+                }
+            }
+        ],
+        text=f"Please record at this time <@{message['user']}>!"
+    )
+    await increase_counter('priority')
+
+
+# Slack MESSAGE Handler: normal
+@bolt.message(re.compile("(hi|hello|hey)", re.I))
+async def say_hello_regex(say, context):
+    greeting = context['matches'][0]
+    await say(f"{greeting}, how are you?")
+    await increase_counter("normal")
+
+
+# Slack COMMAND Handler
 @bolt.command("/help-slack-bolt")
 async def slash_help(ack, body):
     user_id = await body["user_id"]
     await ack(f"Request for help has been sent <@{user_id}>!")
 
+    # Check if the user is allowed to use the command
+    if command['user_id'] in allowed_users:
+        # Respond to the command
+        await say(f"Hello, <@{command['user_id']}>")
+    else:
+        await say(f"Sorry, <@{command['user_id']}>, you are not allowed to use this command.")
 
-# Slash command add-contact
+
+# Slack COMMAND Handler
 @bolt.command("/add-contact")
 async def add_contact(ack, respond, commandadd):
     # Acknowledge command request
@@ -97,7 +185,7 @@ async def add_contact(ack, respond, commandadd):
         respond("Invalid format. Please provide a valid Slack User ID.")
 
 
-# Slash command that retrieves a contact
+# Slack COMMAND Handler
 @bolt.command("/get-contact")
 async def get_contact(ack, respond, commandget):
     # Acknowledge command request
@@ -110,6 +198,7 @@ async def get_contact(ack, respond, commandget):
         respond(f"No contact found for User ID: {user_id}")
 
 
+# Database Function
 async def increase_counter(message_type: str):
     now = datetime.datetime.now()
     formatted_date = now.strftime("%Y-%m-%d")
@@ -134,63 +223,6 @@ async def increase_counter(message_type: str):
         print(f"{message_type} is not a valid key in the document")
 
 
-@bolt.message(re.compile("(asap|help|critical)", re.I))
-async def message_urgent(message, say):
-    await say(
-        blocks=[
-            {
-                "type": "section",
-                "text": {"type": "mrkdwn", "text": f"Do you need help <@{message['user']}>?"},
-                "accessory": {
-                    "type": "button",
-                    "text": {"type": "plain_text", "text": "Click to escalate"},
-                    "action_id": "click_button_notify"
-                }
-            }
-        ],
-        text=f"Hey there <@{message['user']}>!"
-    )
-    await increase_counter('urgent')
-
-
-@bolt.message(re.compile("(important|urgent|soon)", re.I))
-async def message_priority(message, say):
-    await say(
-        blocks=[
-            {
-                "type": "section",
-                "text": {"type": "mrkdwn", "text": f"<@{message['user']}> Can it wait until Monday?"},
-                "accessory": {
-                    "type": "button",
-                    "text": {"type": "plain_text", "text": "Click to record a message"},
-                    "action_id": "click_button_notify"
-                }
-            }
-        ],
-        text=f"Please record at this time <@{message['user']}>!"
-    )
-    await increase_counter('priority')
-
-
-@bolt.message(re.compile("(hi|hello|hey)", re.I))
-async def say_hello_regex(say, context):
-    greeting = context['matches'][0]
-    await say(f"{greeting}, how are you?")
-    await increase_counter("normal")
-
-
-@bolt.event("message")
-async def check_starred(context: AsyncBoltContext, message: dict, starredcontacts: AsyncWebClient):
-    bot_id = context.bot_user_id
-    # Get the list of items starred by the bot
-    result = await starredcontacts.stars_list(user=bot_id)
-    # Check if the received message is in the list of starred items
-    for item in result['items']:
-        if 'message' in item and item['message']['ts'] == message['ts']:
-            print("starred")
-            await increase_counter_based_on_user_id(user_id=bot_id)
-
-
 async def increase_counter_based_on_user_id(user_id: str):
     if user_id in my_contacts.keys():
         await increase_counter("priority")
@@ -198,6 +230,7 @@ async def increase_counter_based_on_user_id(user_id: str):
         await increase_counter("normal")
 
 
+# starts the Socket Mode handler using the AsyncApp instance
 async def main():
     handler = AsyncSocketModeHandler(bolt, os.environ["POETRY_SCM_XAPP_TOKEN"])
     await handler.start_async()
