575550fc5fc983c808c77dbe79f31ec5a9de830a
John Dutchover <1983909-johndutchover@users.noreply.gitlab.com>
2024-01-17 16:37:50 -0500

    streamlit woes

--- frontend/start_streamlit.sh
+++ frontend/start_streamlit.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 # streamlit run pages/1_app.py --server.port=8501 --server.address=$MY_IP_ADDRESS
-streamlit run login.py --server.port=8501 --server.address=$MY_IP_ADDRESS
+.venv/bin/streamlit run login.py --server.port=8501 --server.address=$MY_IP_ADDRESS
